8c8858e6e43e84d1876a2374fe809fb0aa934323
Yasmin <yasminsdcastro@gmail.com>
2022-05-09 10:33:05 -0300

    feat: seo

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,4 +1,5 @@
-import type { LinksFunction } from "@remix-run/node";
+import type { LinksFunction, MetaFunction } from "@remix-run/node";
+import { Link } from "@remix-run/react";
 
 import stylesUrl from "~/styles/index.css";
 
@@ -6,10 +7,26 @@ export const links: LinksFunction = () => {
   return [{ rel: "stylesheet", href: stylesUrl }];
 };
 
-export default function IndexRoute() {
-  return <div>Hello Index Route</div>;
-}
+export const meta: MetaFunction = () => ({
+  title: "Remix: So great, it's funny!",
+  description: "Remix jokes app. Learn Remix and laugh at the same time!",
+});
 
-export function ErrorBoundary() {
-  return <div className="error-container">I did a whoopsies.</div>;
+export default function Index() {
+  return (
+    <div className="container">
+      <div className="content">
+        <h1>
+          Remix <span>Jokes!</span>
+        </h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="jokes">Read Jokes</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
 }
